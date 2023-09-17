a6e770ee05baae83c1bcb771d110b50529473ff0
Artur <alimayko91@gmail.com>
2023-09-17 23:37:28 +0300

    test commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+     Hello, samurai! Let's go! IT-KAMASUTRA
+        <ul>
+            <li>css</li>
+            <li>html</li>
+            <li>react</li>
+        </ul>
     </div>
   );
 }
+
 
 export default App;
