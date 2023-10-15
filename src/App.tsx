0c5579e295b546c89135008564433a8cee849a7d
Artur <alimayko91@gmail.com>
2023-10-15 13:45:16 +0300

    20. add NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
 import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
+import {Settings} from "./components/Settings/Settings";
 import {BrowserRouter, Route} from 'react-router-dom';
 
 
@@ -18,11 +19,11 @@ function App() {
                 {/*<Profile/>*/}
 
                 <div className='app-wrapper-content'>
-                    <Route path='/dialogs' component={Dialogs}/>
                     <Route path='/profile' component={Profile}/>
+                    <Route path='/dialogs' component={Dialogs}/>
                     <Route path='/news' component={News}/>
                     <Route path='/music' component={Music}/>
-
+                    <Route path='/settings' component={Settings}/>
                 </div>
             </div>
         </BrowserRouter>
---
+++ src/components/Settings/Settings.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Settings = () => {
+    return (
+        <div>
+            <h3> This is page without settings = _ =</h3>
+            <ul>
+                <li>one</li>
+                <li>two</li>
+                <li>three</li>
+                <li>fourth</li>
+            </ul>
+        </div>
+    );
+};
