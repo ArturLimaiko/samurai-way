cfad574862d0484d3d9b69627c0309c9d0df51f8
Artur <alimayko91@gmail.com>
2023-10-15 11:56:53 +0300

    17. create page dialogs & code refactoring

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import s from './Profile.module.css'
 import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <div>
                     <img
